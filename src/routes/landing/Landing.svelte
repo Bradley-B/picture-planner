db316438dbb5025b6e99f36b03e70f7f67559ceb
Bradley Boxer <brad.boxer1@gmail.com>
2022-08-08 16:53:59 -0400

    landing page
    this looks pretty good actually

---
+++ src/routes/landing/Landing.svelte
@@ -0,0 +1,66 @@
+<style>
+  * {
+      font-family: sans-serif;
+  }
+
+  div {
+      margin-top: 50px;
+      margin-left: 50px;
+      height: 70vh;
+  }
+
+  h1 {
+      margin-bottom: 30px;
+  }
+
+  img {
+      float: right;
+      margin-right: 100px;
+  }
+
+  p {
+      white-space: pre-line;
+      line-height: 175%;
+  }
+
+  a {
+      margin-top: 20px;
+      padding: 20px 30px 20px 30px;
+      background-color: #bff5bf;
+      border-radius: 5px;
+      display: inline-block;
+  }
+
+  a:hover {
+      background-color: #9ce79c;
+  }
+
+  a:active {
+      border: 1px solid black;
+  }
+</style>
+
+<div id="page">
+  <h1>Hey</h1>
+
+  <img src="https://www.ikea.com/us/en/images/products/yllevad-collage-frame-for-4-photos-black__0809065_pe770938_s5.jpg?f=s" alt="ikea frames on a wall"/>
+
+  <p>
+    So, do you know those picture collages with lots of frames that are all a part of one source image?
+    Like this one from ikea.
+
+    I wanted to make one of those, but I was having trouble visualizing how the picture should look.
+    What parts of the big image should be in frames? I don't know, ya gotta be some kinda design genius.
+
+    Anyway, I decided to make this tool that cuts holes from an image so you can see how the final thing will look.
+    To use it, put some picture frames on the image and click/drag them around.
+
+    It'll tell you the size you should make your background image to maintain the current visual scale.
+    To change the scale, change the "pixels per inch" option in the sidebar or resize your screen.
+
+    Only supports desktop screens because mobile is too small.
+    The jank is part of the charm 💖
+  </p>
+
+  <a href="/planner">Take a look-see</a>
+</div>
