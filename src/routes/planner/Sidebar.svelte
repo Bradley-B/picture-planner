8f57745f1c665cb2f84775081931b4e114fa9d26
Bradley Boxer <brad.boxer1@gmail.com>
2022-08-13 00:50:09 -0400

    fix svg export

--- src/routes/planner/Sidebar.svelte
+++ src/routes/planner/Sidebar.svelte
@@ -60,10 +60,15 @@
   };
 
   const saveSvg = () => {
-    // TODO fix export by converting translate calls to absolute x/y
+    const svg = document.getElementById('svg').cloneNode(true);
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 
-    const svg = document.getElementById('svg');
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    for (let frameRect of svg.querySelectorAll('.frame-rect')) {
+      frameRect.setAttribute('x', '' + parseFloat(frameRect.style.getPropertyValue('--left')));
+      frameRect.setAttribute('y', '' + parseFloat(frameRect.style.getPropertyValue('--top')));
+      frameRect.style.removeProperty('--left');
+      frameRect.style.removeProperty('--top');
+    }
 
     let svgUrl = URL.createObjectURL(
       new Blob(
