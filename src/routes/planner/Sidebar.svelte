35ca08788d7bd55d5e9710479c86a537cacca9f7
Bradley Boxer <brad.boxer1@gmail.com>
2022-08-14 14:08:26 -0400

    make sidebar always open

--- src/routes/planner/Sidebar.svelte
+++ src/routes/planner/Sidebar.svelte
@@ -1,37 +1,8 @@
 <style>
-    #open-button-wrapper {
-        width: 100%;
-        display: flex;
-        justify-content: flex-end;
-    }
-    #open-button {
-        height: 87px;
-        width: 70px;
-        user-select: none;
-        background-color: transparent;
-        border: none;
-        font-size: 4rem;
-        cursor: pointer;
-    }
-    #open-button:hover {
-        background-color: darkgray;
-    }
-    #open-button:active {
-        border: 1px solid black;
-    }
-
     #sidebar {
-        width: auto;
-        height: 100%;
         background-color: wheat;
-        display: grid;
-        grid-template-rows: min-content 10px auto;
-    }
-    #sidebar-body {
         width: 300px;
         height: 100%;
-        margin-left: auto !important;
-        grid-area: 3;
     }
 
     input[type=checkbox] {
@@ -46,10 +17,8 @@
 
 <script>
   import { settings, imageDetails, framesById } from './plannerStores.js';
-  import { slide } from '../../lib/transitions.js';
   import { download, generateCollectionZip, getCanvasFromBlob, getSvgForDownload } from '../../lib/exportFunctions.js';
 
-  let isNavOpen = true;
   let selectedFrameSize;
 
   const onFileSelect = event => {
@@ -105,35 +74,26 @@
 </script>
 
 <div id="sidebar">
-  <div id="open-button-wrapper">
-    <button id="open-button" on:click={() => { isNavOpen = !isNavOpen; }}>{ isNavOpen ? 'X' : '☰' }</button>
-  </div>
+  <label><input type="checkbox" bind:checked={$settings.isMaskEnabled}>Toggle Mask</label><br/>
+  <label>Pixels per inch<input type="number" value={$settings.pixelsPerInch} on:input={onPixelsPerInchInput} min="1"></label>
+  <input type="file" accept=".jpg, .jpeg, .png" on:change={onFileSelect}>
+  <button on:click={() => framesById.addFrame(selectedFrameSize)}>add frame</button>
 
-  {#if isNavOpen}
-    <div id="sidebar-body" transition:slide={{ duration: 500 }}>
+  <select bind:value={selectedFrameSize}>
+    {#each $settings.frameSizes as frameSize}
+      <option value={frameSize}>
+        {frameSize[0]}"x{frameSize[1]}"
+      </option>
+    {/each}
+  </select><br/>
 
-      <label><input type="checkbox" bind:checked={$settings.isMaskEnabled}>Toggle Mask</label><br/>
-      <label>Pixels per inch<input type="number" value={$settings.pixelsPerInch} on:input={onPixelsPerInchInput} min="1"></label>
-      <input type="file" accept=".jpg, .jpeg, .png" on:change={onFileSelect}>
-      <button on:click={() => framesById.addFrame(selectedFrameSize)}>add frame</button>
+  <button disabled={$imageDetails.src === 'default-image.jpg'} on:click={exportSvg}>export as svg</button>
+  <button disabled={$imageDetails.src === 'default-image.jpg'} on:click={exportImage}>export as image</button>
+  <button disabled={$imageDetails.src === 'default-image.jpg'} on:click={exportCollection}>export as collection</button>
 
-      <select bind:value={selectedFrameSize}>
-        {#each $settings.frameSizes as frameSize}
-          <option value={frameSize}>
-            {frameSize[0]}"x{frameSize[1]}"
-          </option>
-        {/each}
-      </select><br/>
-
-      <button disabled={$imageDetails.src === 'default-image.jpg'} on:click={exportSvg}>export as svg</button>
-      <button disabled={$imageDetails.src === 'default-image.jpg'} on:click={exportImage}>export as image</button>
-      <button disabled={$imageDetails.src === 'default-image.jpg'} on:click={exportCollection}>export as collection</button>
-
-      <p>
-        total size is
-        <input type="number" value={displayWidthInches} on:input={onDisplayWidthInput} min="1" />" x
-        <input type="number" value={displayHeightInches} on:input={onDisplayHeightInput} min="1" />"
-      </p>
-    </div>
-  {/if}
+  <p>
+    total size is
+    <input type="number" value={displayWidthInches} on:input={onDisplayWidthInput} min="1" />" x
+    <input type="number" value={displayHeightInches} on:input={onDisplayHeightInput} min="1" />"
+  </p>
 </div>
