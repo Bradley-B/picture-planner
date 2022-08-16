13bc269e6a33304ffc396e97b823ff0e4ff12b23
Bradley Boxer <brad.boxer1@gmail.com>
2022-08-16 00:16:56 -0400

    fix landing page

--- src/routes/landing/Landing.svelte
+++ src/routes/landing/Landing.svelte
@@ -3,10 +3,14 @@
       font-family: sans-serif;
   }
 
-  div {
-      margin-top: 50px;
-      margin-left: 50px;
-      height: 70vh;
+  .page {
+      height: 100%;
+      overflow: auto;
+  }
+
+  .content {
+      margin: 50px;
+      height: fit-content;
   }
 
   h1 {
@@ -14,53 +18,62 @@
   }
 
   img {
-      float: right;
-      margin-right: 100px;
+      display: block;
+      margin: 0 auto 0 auto;
+      max-width: 100%;
   }
 
   p {
       white-space: pre-line;
       line-height: 175%;
+      width: 100%;
   }
 
-  a {
-      margin-top: 20px;
+  .button {
+      margin: 20px 0 50px 0;
       padding: 20px 30px 20px 30px;
-      background-color: #bff5bf;
+      background-color: #534D41;
       border-radius: 5px;
       display: inline-block;
+      color: #F5DFBB;
+      text-decoration: none;
   }
 
-  a:hover {
-      background-color: #9ce79c;
+  .button:hover {
+      background-color: #454036;
   }
 
-  a:active {
+  .button:active {
       border: 1px solid black;
+  }
+
+  .content-inner {
+      max-width: 90ch;
   }
 </style>
 
-<div id="page">
-  <h1>Hey</h1>
+<div class="page">
+  <div class="content">
+    <h1>Hey</h1>
+    <div class="content-inner">
+      <p>
+        So, do you know those picture collages with lots of frames that are all a part of one source image?
 
-  <img src="https://www.ikea.com/us/en/images/products/yllevad-collage-frame-for-4-photos-black__0809065_pe770938_s5.jpg?f=s" alt="ikea frames on a wall"/>
+        <a href="https://www.ikea.com/us/en/p/yllevad-collage-frame-for-4-photos-black-50429768/">Like this one from ikea:</a>
+        <img src="ikea-frames.jpg" alt="ikea frames on a wall"/>
 
-  <p>
-    So, do you know those picture collages with lots of frames that are all a part of one source image?
-    Like this one from ikea.
+        I wanted to make one of those, but I was having trouble visualizing how the picture should look.
+        What parts of the big image should be in frames? I don't know, ya gotta be some kinda design genius.
 
-    I wanted to make one of those, but I was having trouble visualizing how the picture should look.
-    What parts of the big image should be in frames? I don't know, ya gotta be some kinda design genius.
+        Anyway, I decided to make this tool that cuts holes from an image so you can see how the final thing will look.
+        To use it, put some picture frames on the image and click/drag them around.
 
-    Anyway, I decided to make this tool that cuts holes from an image so you can see how the final thing will look.
-    To use it, put some picture frames on the image and click/drag them around.
+        Play around with the different settings, it's pretty cool. You can export your creation whole as an svg/image,
+        or broken up into separate images according to your frames.
 
-    It'll tell you the size you should make your background image to maintain the current visual scale.
-    To change the scale, change the "pixels per inch" option in the sidebar or resize your screen.
-
-    Only supports desktop screens because mobile is too small.
-    Now with 30% less jank 💖
-  </p>
-
-  <a href="/planner">Take a look-see</a>
+        Now with 35% less jank 💖
+      </p>
+      <a class="button" href="/planner">Take a look-see</a>
+    </div>
+  </div>
 </div>
